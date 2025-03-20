c55b3e6f0935e23487213bf6a2e393475587345d
help12138 <18736993700@163.com>
2025-03-20 18:49:29 +0800

    用链式解决哈希冲突

--- STLDemo/array_hash.cpp
+++ STLDemo/array_hash.cpp
@@ -10,7 +10,11 @@ struct Pair
 		this->val = val;
 	}
 };
-
+/*
+***************************************
+* *******有哈希冲突版本****************
+* *************************************
+*/
 // 基于数组实现的哈希表
 class ArrayHashMap
 {
@@ -109,13 +113,120 @@ ArrayHashMap::~ArrayHashMap()
 	buckets.clear();
 }
 
+
+
+/*
+***************************************
+* *******无哈希冲突版本****************
+* *************************************
+*/
+
+class HashMapChaining {
+private:
+	int size;  // 键值对数量
+	int capacity;  // 哈希表容量
+	double loadThres;  // 扩容阈值
+	int extendRatio;  // 扩容倍数
+	vector<vector<Pair*>> buckets;  // 桶数组
+public:
+	HashMapChaining() :size(0), capacity(4), loadThres(2.0 / 3.0), extendRatio(2) {
+		buckets.resize(capacity);
+	}
+
+	~HashMapChaining(){
+		for (auto& bucket : buckets) {
+			for (Pair* pair : bucket) {
+				delete pair;
+			}
+		}
+	}
+
+	int hasFunc(int key) {
+		return key % capacity;
+	}
+
+	// 负载因子
+	double loadFactor() {
+		return (double)size / (double)capacity;
+	}
+
+	// 扩容
+	void extend() {
+		vector<vector<Pair*>> bucketsTmp = buckets;
+		capacity *= extendRatio;
+		buckets.clear();
+		buckets.resize(capacity);
+		size = 0;
+		for (auto& bucket : bucketsTmp) {
+			for (Pair* pair : bucket) {
+				put(pair->key, pair->val);
+				delete pair;
+			}
+		}
+	}
+
+	// 查询
+	string get(int key) {
+		int index = hasFunc(key);
+		for (Pair* pair : buckets[index]) {
+			if (pair->key == key) {
+				return pair->val;
+			}
+		}
+		return "";
+	}
+
+	// 添加
+	void put(int key, string val) {
+		// 当负载因子超过阈值时, 执行扩容
+		if (loadFactor() > loadThres) {
+			extend();
+		}
+		int index = hasFunc(key);
+		for (Pair *pair: buckets[index]) {
+			if (pair->key == key) {
+				// 有则更新
+				pair->val = val;
+			}
+		}
+		buckets[index].push_back(new Pair(key, val));
+		size++;
+
+	}
+	// 删除
+	void remove(int key) {
+		int index = hasFunc(key);
+		auto& bucket = buckets[index];
+		for (int i = 0; i < bucket.size(); i++) {
+			if (bucket[i]->key == key) {
+				Pair* tmp = bucket[i];
+				bucket.erase(bucket.begin() + i);
+				delete tmp;
+				size--;
+				return;
+			}
+		}
+	}
+	// 打印
+	void print() {
+		for (auto& bucket : buckets) {
+			cout << "[";
+			for (Pair* pair : bucket) {
+				cout << pair->key << " -> " << pair->val << ", ";
+			}
+			cout << "]\n";
+		}
+	}
+
+};
+
 int main() {
 	ArrayHashMap map;
 	map.put(1, "man");
 	map.put(2, "man");
 	map.put(3, "man");
 	map.put(4, "man");
-	map.remove(4);
+	map.remove(2);
 	map.print();
 	return 0;
 }
