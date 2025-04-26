17f5e16abb164decd56f6218e82426a85f9da5d4
help12138 <18736993700@163.com>
2025-04-26 21:02:23 +0800

    函数参数

--- C++PrimerLearn/func.cpp
+++ C++PrimerLearn/func.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>
 
 #include "func.h"
 
-/*函数的源代码功能定义， 括号里的为形参， 形参的生命周期只到函数大括号结束*/
+/*函数的源代码功能定义， 括号里的为形参， 形参的生命周期只到函数大括号结束
+* 参数传递时， 如果形参不是引用类型， 都是拷贝一个副本，引用类型传递的则是
+* 本值， C语言里也可以传递指针类型形参， 修改本值，但传递指针类型也是拷贝了
+* 有个指针副本， 只不过指向的同一块内存
+*/
 int func(int val){
     // 计算从0~val的阶乘
     //static定义的参数生命周期为全局
@@ -19,19 +24,34 @@ int func(int val){
     return resultNum;
 }
 
+/*数组参数传递的是指针*/
+int pointFunc (const int* num){
+    if(num){
+        while (*num)
+        {
+            cout << *num++ << " ";
+        }
+    }else{
+        cout << "question";
+    }
+
+    return 0;
+}
+
 int main(){
     // 函数调用
-    int val = 4;
-    int result;
-    /*此处的val为实参，顾名思义，实际的参数*/ 
-    for (int i = 0; i < 3; i++)
-    {
-        func(1);
-    }
     
+
+    // /*此处的val为实参，顾名思义，实际的参数*/ 
+    // result = func(val);
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     //验证静态参数的生命周期
+    //     func(1);
+    // }
     
-    result = func(val);
-    std::cout << "the func return is " << result << std::endl;
-    
+    int list[] = {1,2,34,5,6,7,8,9,0};
+    pointFunc(list);
+
     return 0;
 }
--- C++PrimerLearn/func.h
+++ C++PrimerLearn/func.h
@@ -2,5 +2,5 @@
 #define HEAD_
 /* 头文件放置函数的声明，在源文件引用，可以解决声明排布问题 */
 int func(int val);
-
+using namespace std;
 #endif
